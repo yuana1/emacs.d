9fd6bd2a1b6b8f5b2e2f2c4033ea5652c6b2d842
zhaokaiyuan <zhaoky0918@sina.com>
2020-01-01 23:05:18 +0800

    添加flymake

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,5 +1,9 @@
 (require-package 'lsp-mode)
 
+(after-load 'lsp-mode
+	    (add-hook 'lsp-mode 
+		      (lambda() (flymake-mode t)))
+	    )
 (require-package 'lsp-ui )
 (require-package 'company-lsp ) 
 (require-package 'lsp-ivy )
