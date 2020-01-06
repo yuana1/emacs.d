2119f6ef72831eaf194d61cc1f995ccdd28b11b6
zhaokaiyuan <zhaoky0918@gmail.com>
2020-01-06 14:02:14 +0800

    添加 set mark command

--- init.el
+++ init.el
@@ -42,7 +42,18 @@
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; 一些配置
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tool-bar-mode nil)
+ )
 
+(global-set-key [(ctrl f12)] 'set-mark-command)
+
+;;(global-set-key (kbd "C-f1") 'set-mark-command)
 (require 'init-utils)
 (require 'init-elpa)
 (require 'init-ibuffer)
