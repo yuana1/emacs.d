9dca78c066b8daf290f4ca9677f3377d5876b443
zhaokaiyuan <zhaoky0918@sina.com>
2020-01-06 22:16:13 +0800

    add java dapmode

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -2,7 +2,11 @@
 
 (after-load 'lsp-mode
   (add-hook 'lsp-mode 
-	    (setq lsp-prefer-flymake nil))
+	    (setq lsp-prefer-flymake nil)
+            (setq dap-mode t)
+            (setq dap-ui-mode t)
+            )
+
   )
 (require-package 'lsp-ui )
 (require-package 'company-lsp ) 
@@ -10,6 +14,6 @@
 (require-package 'lsp-treemacs )
 ;; optionally if you want to use debugger
 (require-package 'dap-mode)
-
+(require 'dap-java)
 
 (provide 'init-lsp)
